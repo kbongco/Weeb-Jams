815c9291131290bfaa67659ee0eca18ef83fb3e4
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-10-23 15:16:45 -0400

    added in api for getting specific anime

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
 import { useState } from 'react'
+import axios from 'axios';
 import Input from './Components/Input'
 import './App.css'
+import { sendJikanData, getAnimeTheme } from './constants';
+
 
 function App() {
-  const [count, setCount] = useState(0)
   const [inputValue, setInputValue] = useState('');
+  const [animeData, setAnimeData] = useState<any>({});
+  const [filteredAnime, setFilteredAnime] = useState<any>([]);
+  const [animeId, setAnimeId] = useState(null);
+  const [animeTheme, setAnimeThemes] = useState([]);
+
   const handleInputChange = (newValue: string) => {
     setInputValue(newValue);
   };
+
+  const getAnime = () => {
+    axios.get(sendJikanData(inputValue)).then((response) => {
+      setAnimeData(response.data);
+      const malIdAndTitles = animeData?.data?.map((anime:any) => ({
+        mal_id: anime.mal_id,
+        title: anime.title,
+      }));
+    
+      const filteredMalIdAndTitles = malIdAndTitles?.filter((anime:any) => {
+        return anime?.title?.toLowerCase().includes(inputValue.toLowerCase());
+      });
+
+      setFilteredAnime(filteredMalIdAndTitles);
+    }).catch((error) => {
+      console.error(error);
+    });
+  };
+
+  const handleAnimeClick = (malId:any) => {
+    setAnimeId(malId);
+  }
+
 
   return (
     <>
@@ -18,7 +48,34 @@ function App() {
           value={inputValue}
           onChange={handleInputChange}
         />
-      </div>
+        <button onClick={getAnime}>Get Anime</button>
+        {
+        filteredAnime && filteredAnime.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredAnime.map((anime: any) => (
+                <tr key={anime.mal_id}>
+                  <td>{anime.title}</td>
+                  <td>
+                    <button onClick={() => handleAnimeClick(anime.mal_id)}>
+                      Select
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <h1>No anime found</h1>
+        )
+      }
+    </div>
     </>
   )
 }
---
+++ src/constants.tsx
@@ -0,0 +1,7 @@
+export const sendJikanData = (inputValue: string) => {
+  return `https://api.jikan.moe/v4/anime?q=${inputValue}`
+}
+
+export const getAnimeTheme = (id: any) => {
+  return `https://api.jikan.moe/v4/anime/${id}/themes`
+}
